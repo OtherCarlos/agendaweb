65a48d1dc214be93929cc2911742a4768bf3783c
Alejandro <alejandro@alejandro-M2420>
2017-10-28 18:52:06 -0400

    Js Profile

--- js/util.js
+++ js/util.js
@@ -1,3 +1,7 @@
+var http = location.protocol;
+var slashes = http.concat("//");
+var baseUrl = slashes.concat(window.location.hostname);
+
 $(document).ready(function(){
 	$('a[href^="#"]').on('click',function (e) {
 	    e.preventDefault();
@@ -11,4 +15,19 @@ $(document).ready(function(){
 	        window.location.hash = target;
 	    });
 	});
+
+
+        $('#button_editar').click(function(){
+
+                $('#button_editar').addClass('disabled', 'disabled-grey');
+
+                $('#UserProfile_nombres').attr('readonly', false);
+                $('#UserProfile_apellidos').attr('readonly', false);
+                $('#UserProfile_direccion').attr('readonly', false);
+                $('#UserProfile_ciudad').attr('readonly', false);
+                $('#UserProfile_pais').attr('readonly', false);
+                $('#UserProfile_sobre_mi').attr('readonly', false);       
+                $('#button_guardar').show();        
+        });
+        
 });
